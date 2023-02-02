c7d92059292ad1e1c8f06e446dcf1ed54c9469a4
giulia-colombo <giulia.colombo.127@gmail.com>
2023-02-02 18:01:07 +0100

    fixed PartyCard with buttons and populated club. Tested all functionalities - working

--- src/components/PartyCard.jsx
+++ src/components/PartyCard.jsx
@@ -4,12 +4,13 @@ import { useContext } from "react";
 
 import { AuthContext } from "../context/auth.context";
 import axios from "axios";
+import partyPlaceholder from "../assets/default-avatar.png"
 
 const API_URL = "http://localhost:5005";
-//const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
+// const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
 const PartyCard = (props) => {
-    const {name, club, date, musicGenre, image, attendees, owner, _id} = props;
+    const {name, club, date, musicGenre, image, attendees, owner, _id, getAllParties} = props;
     // const [attendees, setAttendees] = useState([]);
     const partyId = _id;
     const navigate = useNavigate();
@@ -19,31 +20,38 @@ const PartyCard = (props) => {
     const deleteParty = () => {
         //make a DELETE request to delete the Party
         axios.delete(`${API_URL}/api/party/${partyId}`)
+        .then(() => getAllParties())
     // Once the delete request is resolved successfully, navigate back to the list of parties
         .then(() => navigate("/party"))
         .catch((err) => console.log(err));
     };
 
     const attendParty = () => {
-        axios.put(`${API_URL}/party/${partyId}/attend-party`, user._id)
+        const storedToken = localStorage.getItem('authToken');
+
+        axios.put(`${API_URL}/api/party/${partyId}/attend-party`, {userId : user._id} , {headers: {Authorization: `Bearer ${storedToken}`}})
+        .then(() => getAllParties())
         .then(() => console.log("attendee is joining"))
         .catch((err) => console.log(err))
     }
 
     const leaveParty = () => {
-        axios.put(`${API_URL}/party/${partyId}/leave-party`, user._id)
+        const storedToken = localStorage.getItem('authToken');
+
+        axios.put(`${API_URL}/api/party/${partyId}/leave-party`, {userId : user._id}, {headers: {Authorization: `Bearer ${storedToken}`}})
+        .then(() => getAllParties())
         .then(() => console.log("attendee is leaving"))
         .catch((err) => console.log(err))
     }
     
     return (
-        <div className="PartyCard">
+        <div className="PartyCard" key={_id}>
             <h3>{name}</h3>
-            <h4>Location: {club}</h4>
-            <img src={image} alt="partyImage" />
+            <h4>Location: {club.name}</h4>
+            <img src={image !== "" ? image : partyPlaceholder} alt="partyImage" />
             <p>Date: {date}</p>
             <p>Music genre: {musicGenre}</p>
-            <p>Party host: {owner}</p>
+            <p>Party host: {owner.name}</p>
             <h5>Attendees: <span>{attendees.length}</span></h5>
             {attendees.length > 0 && 
             <ul>
@@ -67,7 +75,7 @@ const PartyCard = (props) => {
 
             {/* the edit/delete buttons below need to be rendered only if current user is the user who created that party, and then make a request to the specific API  */}
             {   
-                user._id === owner &&
+                user._id === owner._id &&
                 <>
                 <Link to={`/party/${partyId}/edit`}>
                     <button>Edit this party</button>
